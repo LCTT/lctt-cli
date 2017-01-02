02a506bd262b46daa0674ec550aa5da3eef0b0d6
WenkangJi <erlinux@users.noreply.github.com>
2017-01-02 16:16:01 +0800

    Update select.sh

--- select.sh
+++ select.sh
@@ -50,8 +50,7 @@ function first_display() {
 ##这里是主代码区
 enter_dir	# 确定用户在 “TranslateProject” 目录
 first_display	# 给用户展示 “Linux中国翻译规范” 文件
-maths_now	# 告知用户 “LCTT” 总共有多少文件（全部）
 find_translated	# 综合 "keyword" 至 /tmp/TranslateProject.txt 文件
-$(command -v echo) -e "\n"  && cat /tmp/TranslateProject.txt | less
-		# 告知用户 “LCTT” 剩余有多少文章（未被翻译）
-translated	# 告知用户 “LCTT” 剩余有多少文章（被翻译）	
+$(command -v echo) -e "\n"  && cat /tmp/TranslateProject.txt | less	# 告知用户 “LCTT” 剩余有多少文章（未被翻译）
+maths_now	# 告知用户 “LCTT” 总共有多少文件（全部文章）
+translated	# 告知用户 “LCTT” 剩余有多少文章（被翻译）
