40ff938e83e108024b77dca78c89dd375188d44b
erlinux <jiwenkangatech@foxmail.com>
2017-01-24 08:34:01 -0500

    cancel and delete something

--- gn2.sh
+++ gn2.sh
@@ -26,13 +26,9 @@
 
 # Get Github User detail
   read -p "Github User: " Uname
-# read -p "Github Mail: " Email
-  #! Warm: Test User INPUT
   git clone https://$Uname@github.com/$Uname/TranslateProject 
-  #! Warm??Please add Key login later
+  #! Warm: Please add Key login later
   cd $PWD/TranslateProject
-# git config --global user.name  "$Uname"
-# git config --global user.email "$Email"
   echo -e "\e[1;32mlctt-cli was got ready\e[0m"
   echo -e "\n\e[1;42m Press any key to continue \e[0m"
   read && clear && echo -e '\n\n\n'
@@ -42,20 +38,12 @@
   $(cat "$(find . -inum $(ls -i |grep LCTT | \
   awk '{print $1}'))" | head -n 1)\033[39;49;0m"
   cat "$(find . -inum $(ls -i |grep LCTT | awk '{print $1}'))" | grep -vi \#
-  #??Warm: Not has not do it...
 
 # Show the thime and Say think you
   echo -e "\e[1;33m\nWelcome, "$Uname"! "$(date "+Time Now: %T")"\e[0m\n"
   #! Warm: Later add morning afternoon
 
 # Find File.
-# KEYWORDS="*translated|*translating|*fanyi|*??????|*???뷭??"
-# cd $PWD/../TranslateProject/sources/tech/
-# grep -RHEvi ${KEYWORDS} * | grep -Ev "LCTT?????淶.md|.git|README" | \
-# grep -RHEvi ${KEYWORDS} * | grep -Ev "$(find . -type d | sed 's#[^/]*/##' | \
-# awk -F '[/]' '{print $2}')|LCTT?????淶.md|.git|README" | \
-#  cut -d "/" -f2  | awk -F '.md:' '{print $1}' | sort -u | sed -s 's/$/.md/'
-# sed -s 's/^/"/;s/$/",/' | less 
   if [[ -d "$PWD/../TranslateProject/sources/tech/" ]]; then
     tech="$PWD/../TranslateProject/sources/tech/"
     tech_num=$(find  $tech  -type f | grep -v README.md |  wc -l)
@@ -68,26 +56,22 @@
   fi
 
 # Show File.
-  cat /dev/null > /tmp/TranslateProject_talk.txt
-  cat /dev/null > /tmp/TranslateProject_tech.txt
-  cat /dev/null > /tmp/TranslateProject.txt
   echo -e "\n\e[1;42m Press any key to continue \e[0m"
   read 
 
-  for i in translated translating fanyi ?????? ???뷭?? 
+  for i in translated translating fanyi 翻译
   do
-  cd $talk && grep -RHi $i *| cut -d ":" -f 1 >> /tmp/TranslateProject_talk.txt 
-  cd $tech && grep -RHi $i *| cut -d ":" -f 1 >> /tmp/TranslateProject_tech.txt
+  cd $talk && grep -RHi $i *| cut -d ":" -f 1 > /tmp/TranslateProject_talk.txt 
+  cd $tech && grep -RHi $i *| cut -d ":" -f 1 > /tmp/TranslateProject_tech.txt
   done
 
   find $tech -type f |grep -v README.md |\
   grep -v "$(cat /tmp/TranslateProject_tech.txt |\
-  sort -u )" >> /tmp/TranslateProject.txt
+  sort -u )" > /tmp/TranslateProject.txt
   find $talk -type f |grep -v README.md |\
   grep -v "$(cat /tmp/TranslateProject_talk.txt |\
   sort -u )" >> /tmp/TranslateProject.txt
 
-#  awk -F '[..]' '{print $3,$4,$5,$6}' /tmp/TranslateProject.txt | sed 's/    //' |\
   awk -F '[..]' '{print $3,$4,$5,$6}' /tmp/TranslateProject.txt |\
   sed 's/md//g'| sed 's#^\/TranslateProject\/sources##g' | sed 'sO^[\/]'OOg |\
   sed 's/^/.\//' |  tr -d $ | sed  's/[ \s ]*$//' |\
@@ -111,7 +95,7 @@
     $(sed -i "1i **translating by [$Uname](https://github.com/$Uname)**" \
 	"$(find . -name "$Find_Number" | sed  's#^\.\/##')")
     git add "$(find . -name "$Find_Number" | sed  's#^\.\/##')"
-    git commit -am "By "$Uname" translating"
+    git commit -am "Translating By "$Uname" "
     git push origin master
 
   test $? -eq 0 && {
@@ -123,6 +107,3 @@
   echo -e "\e[1;33mThanks @"$Uname" https://linux.cn/\e[0m\n"
   }
   fi
-
-# 此版本确实为v2.1.2，做了大改变。
-## 包括：修复正则表达，交互效果明显。
