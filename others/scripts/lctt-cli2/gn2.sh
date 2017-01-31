e2b2603168c1457e24803c35a778c30030061c28
erlinux <jiwenkangatech@foxmail.com>
2017-01-31 22:16:34 +0800

    完成框架，实现功能中

--- others/scripts/lctt-cli2/gn2.sh
+++ others/scripts/lctt-cli2/gn2.sh
@@ -1,13 +1,32 @@
 #!/bin/bash
 # Help To Linux'CN Translate Team 
 # Filename: $0  Version: 3.1.2  Author:jiwenkangatech@foxmail.com
-  
+
+  if which tput >/dev/null 2>&1; then                                                  
+      ncolors=$(tput colors)                                                           
+  fi                                                                                   
+  if [ -t 1 ] && [ -n "$ncolors" ] && [ "$ncolors" -ge 8 ]; then                       
+    RED="$(tput setaf 1)"                                                              
+    GREEN="$(tput setaf 2)"                                                            
+    YELLOW="$(tput setaf 3)"                                                           
+    BLUE="$(tput setaf 4)"                                                             
+    BOLD="$(tput bold)"                                                                
+    NORMAL="$(tput sgr0)"                                                              
+  else                                                                                 
+    RED=""                                                                             
+    GREEN=""                                                                           
+    YELLOW=""                                                                          
+    BLUE=""                                                                            
+    BOLD=""                                                                            
+    NORMAL=""                                                                          
+  fi   
+
 # Exit on exceptions.
 set -e
 
-# Environment variable.
-#  echo "this need to find user's translateproject"
-#  echo "and put it in the environment"
+# Head and thanks
+USER=$(git config --list | awk -F "name=" '{print $2}' | xargs)
+printf "Hi~ $USER ${BOLD}$(date "+Time Now: %T")${NORMAL}\n"
 
 # GetNewOne Usage
 usage(){
--- others/scripts/lctt-cli2/options/check/check.sh
+++ others/scripts/lctt-cli2/options/check/check.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
-echo check
+
+# Exit the exoeption.
+  set -e
+
+# Check Soft whether exised.
+  for git in soft
+  do
+    hash $soft 1>/dev/null 2>&1 || {
+    echo "Error: COMMAND $soft is not esised"
+    exit 1
+    }
+  done
+
+# Set Chinese Language
+  LANGUAGE1=$(echo $LANG | awk -F "." '{ print $1 }')
+  LANGUAGE2=$(echo $LANG | awk -F "." '{ print $2 }')
+  test $LANGUAGE1 = zh_CN || echo "Error: OS Language $LANGUAGE1" 
+  test $LANGUAGE2 = UTF-8 || echo "Error: OS Language $LANGUAGE2" 
