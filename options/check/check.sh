ed7dc465cd614e2cfbc6fcff4a2cf847e4fa25dd
erlinux <jiwenkangatech@foxmail.com>
2017-02-01 20:27:36 +0800

    添加readlink检测与仓库存在判断

--- options/check/check.sh
+++ options/check/check.sh
@@ -4,10 +4,9 @@
   set -e
 
 # Check Soft whether exised.
-  for git in soft
-  do
-    hash $soft 1>/dev/null 2>&1 || {
-    echo "Error: COMMAND $soft is not esised"
+  for SOFT in git readlink;do
+    hash $SOFT 1>/dev/null 2>&1 || {
+    echo "Error: COMMAND $SOFT is not esised"
     exit 1
     }
   done
@@ -17,3 +16,9 @@
   LANGUAGE2=$(echo $LANG | awk -F "." '{ print $2 }')
   test $LANGUAGE1 = zh_CN || echo "Error: OS Language $LANGUAGE1" 
   test $LANGUAGE2 = UTF-8 || echo "Error: OS Language $LANGUAGE2" 
+
+# Check repositories
+ git config --list | awk -F "name=" '{print $2}' | xargs >/dev/null 2>&1
+ if [ ! $? -eq 0  ];then
+ echo "Error: REPOSITORIES is not found"
+ fi
