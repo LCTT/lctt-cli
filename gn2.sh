3cfd54b0063554bb017089fe002c2889ea030299
erlinux <jiwenkangatech@foxmail.com>
2017-03-17 22:00:45 +0800

    Delete ColorCodes

--- gn2.sh
+++ gn2.sh
@@ -2,25 +2,6 @@
 # Help To Linux'CN Translate Team 
 # Filename: $0  Version: 3.1.2  Author:jiwenkangatech@foxmail.com
 
-  if which tput >/dev/null 2>&1; then                                                  
-      ncolors=$(tput colors)                                                           
-  fi                                                                                   
-  if [ -t 1 ] && [ -n "$ncolors" ] && [ "$ncolors" -ge 8 ]; then                       
-    RED="$(tput setaf 1)"                                                              
-    GREEN="$(tput setaf 2)"                                                            
-    YELLOW="$(tput setaf 3)"                                                           
-    BLUE="$(tput setaf 4)"                                                             
-    BOLD="$(tput bold)"                                                                
-    NORMAL="$(tput sgr0)"                                                              
-  else                                                                                 
-    RED=""                                                                             
-    GREEN=""                                                                           
-    YELLOW=""                                                                          
-    BLUE=""                                                                            
-    BOLD=""                                                                            
-    NORMAL=""                                                                          
-  fi   
-
 # Exit on exceptions.
 set -e
 
@@ -30,7 +11,8 @@ printf "Hi! $USER ${BOLD}$(date "+Time Now: %T")${NORMAL}\n"
 
 # GetNewOne Usage
 usage(){
-#  echo "Usage: $0 COMMAND [OPTION...]           "
+  echo "Online help: <https://github.com/lctt/lctt-cli>"
+  echo "                                        "
   echo "Usage: $0 [PARAMETER...]                 "
   echo "                                        "
   echo "Parameters:                               "
@@ -40,8 +22,6 @@ usage(){
   echo "  --help      Show help Messages          "
   echo "For example:                            "
   echo "  $0 --help                               "
-  echo "                                        "
-  echo "Online help: <https://github.com/lctt/lctt-cli>"
   echo "                                        "
 }
 
