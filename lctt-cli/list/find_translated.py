c96a760a60547d4d14322955194ea54ebaa57ad2
WenkangJi <erlinux@users.noreply.github.com>
2017-05-16 23:03:23 +0800

    0.0.3
    Version: 0.0.3
    # 解决用户输入问题，优化修复一些小问题。

--- lctt-cli/list/find_translated.py
+++ lctt-cli/list/find_translated.py
@@ -20,25 +20,23 @@ class create_and_del:
             os.remove(self.file)
 
 def list_and_find():
-    list_num=0
     rootdir=os.path.dirname(__file__)+'/TranslateProject/sources/'
     # 三个参数：分别返回1.父目录 2.所有文件夹名字（不含路径） 3.所有文件名字
     for parent,dirnames,filenames in os.walk(rootdir):
+        # 单个进行处理
         for filename in filenames:
             # 排除 "README.md" 后输出
             if 'README.md' not in filename:
                 # 将文件位置进行列出
                 source_strings=os.path.join(parent,filename).replace('\\','/')   # 完整路径
-                source_string=filename                                           # 仅文件名
                 # 将完整路径进行读取
                 with open(source_strings,'r',encoding='utf-8') as files:
                     # 仅仅读取首行
                     read_of_translating=files.readlines()[0]
-                    list_num+=1
                     translated=re.findall(r'(?i)(translated|translating|fanyi|翻译中|申请翻译|github.com)',read_of_translating)
                     if translated == []:
-                        write=str(list_num)+' '+filename
+                        write=filename
                         for write in list(write.split(',')):
                             create_and_del().create_and_putfiles(write+'\n')
                             print(write)
-                        files.close()
+                    files.close()
