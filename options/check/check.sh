cf84894a6ecd06fecba616cf9998e0e637773ab6
erlinux <jiwenkangatech@foxmail.com>
2017-12-12 00:08:14 +0800

    将check命令检测修改为not_install

--- options/check/check.sh
+++ options/check/check.sh
@@ -7,14 +7,14 @@
   for SOFT in git readlink ssh
   do
     hash $SOFT 1>/dev/null 2>&1 || {
-        echo "Error: COMMAND $SOFT is not esised"
+        echo "Error: COMMAND $SOFT is not install"
         exit 1
     }
   done
 
 # Set Chinese Language.
   LANGUAGE=$(echo $LANG | awk -F "." '{ print $1 }')
-  test $LANGUAGE = 'zh_CN' ||  test $LANGUAGE = 'UTF-8' || printf "Error: OS Language $LANGUAGE \nYou can try to set 'LANG=zh_CN.UTF-8'\nWarning: Query result maybe happen wroing becaue OS not support Chinese Langagues.\n" 
+  test $LANGUAGE = 'zh_CN' ||  test $LANGUAGE = 'UTF-8' || printf "Error: OS Language $LANGUAGE \nYou can try to set 'LANG=zh_CN.UTF-8'\nWarning: Query result maybe happen wroing becaue OS not support Chinese Langagues.\n"
 
 # Check USER NAME EXIST.
   if [ -z ${LCTT_USER} ];then
