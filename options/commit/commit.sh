96801d2ae111ffbfc5f67316f4872b7c2beb1d2b
erlinux <jiwenkangatech@foxmail.com>
2018-07-10 13:58:38 +0800

    变量修复代码修补

--- options/commit/commit.sh
+++ options/commit/commit.sh
@@ -4,22 +4,23 @@
 
 # var.
   LCTT_USER=$USERNAME
+  LCTT_PROJECT=$PROJECT
 
 # 接受用户输入
   read -p "Please enter the number: " SELECT
-  NUMBER=$(cat /tmp/aa.txt | sed -n "$SELECT p" | xargs -0)
-  NUMBER_PATH=$(find / -iname "$NUMBER" 2>/dev/null)
+  NUMBER=$(cat $(echo $NWF) | sed -n "$SELECT p" | xargs -0)
+  NUMBER_PATH=$(find $(echo $LCTT_PROJECT) -iname "$NUMBER" 2>/dev/null)
   echo "$NUMBER_PATH"
 
 # 切换到 TranslateProject 项目位置
   cd $(dirname "$NUMBER_PATH")
   # 选中文件并提交给 Github 等待进一步处理
-  sed -i "1i **translating by [$LCTT_USER](https://github.com/$LCTT_USER)** \n\n" "$NUMBER_PATH"
+  sed -i "1i **translating by [$(echo ${LCTT_USER})](https://github.com/$(echo ${LCTT_USER}))** \n\n" "$NUMBER_PATH"
   git_time=$(date +%s)
   git branch ${git_time} master
   git checkout ${git_time}
   git add "$NUMBER_PATH"
-  git commit -am "Translating By $LCTT_USER "
+  git commit -am "Translating By $(echo ${LCTT_USER}) "
   git push origin ${git_time}
   git checkout master
 
@@ -27,8 +28,6 @@
   test $? -eq 0 && {
   echo -e "\e[1;33m   Done.\e[0m"
   echo -e "\e[1;33mNext Pull New Requert,Please\e[0m"
-  echo -e "\033[37;36;4mhttps://github.com/$LCTT_USER/TranslateProject/pull/new/master\
-  \033[39;49;0m\n"
   read && clear
-  echo -e "\e[1;33mThanks @"$LCTT_USER" https://linux.cn/\e[0m\n"
+  echo -e "\e[1;33mThanks @$(echo ${LCTT_USER}) https://linux.cn/\e[0m\n"
   }
--- options/list/list.sh
+++ options/list/list.sh
@@ -5,12 +5,17 @@
 # var.
   LCTT=$PROJECT
   LCTT_USER=$USERNAME
+  export ODF=$(mktemp)
+  export NWF=$(mktemp)
 
 # 切换到 TranslateProject 项目位置
   cd $LCTT/sources/
   # 找到未被翻译文章并列出
-  ``grep -RHEi "translated|translating|fanyi|翻译中|申请翻译" | \
-  awk -F ":" '{print $1}' | awk -F "/" '{print $2}'`` > /tmp/aaa.txt
-  ``find -type f 2>/dev/null | sed -e "s#^[ . ]/##" | awk -F '/' '{print $2}' | grep -v yearbook2015 | \
-  grep -v README.md``| grep -v "$( cat  /tmp/aaa.txt)"`` > /tmp/aa.txt
-  sed '/^$/d' /tmp/aa.txt | cat -n | less
+  grep -RHEi "translated|translating|fanyi|翻译中|申请翻译" | \
+  awk -F ":" '{print $1}' | awk -F "/" '{print $2}' > $(echo $ODF)
+  find -type f 2>/dev/null | sed -e "s#^[ . ]/##" | awk -F '/' '{print $2}' | grep -v yearbook2015 | \
+  grep -v README.md``| grep -v "$( cat $(echo $ODF))" > $(echo $NWF)
+  sed '/^$/d' $(echo $NWF) | cat -n | less
+
+bash "$(dirname $0)/../commit/commit.sh"
+test $? -eq 0 && exit 0
